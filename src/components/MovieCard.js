2eb71c6039c46b07b0d0ea9c225391a7775afd17
Fimil Faneea <fimil.faneea@gds.ey.com>
2025-05-14 19:21:09 +0530

    search

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -7,13 +7,26 @@ export default function MovieCard({ movie }) {
         e.target.src = "images/default.jpg"; // Fallback image
 
     }
+
+    const getRating = (rating) => {
+        if (rating >= 8) {
+            return 'rating-good';
+        } 
+        else if (rating >= 5) {
+            return 'rating-ok';
+        } else {
+            return 'rating-bad';
+        }
+    };
+
     return (
         <div className="movie-card">
             <img src={`images/${movie.image}`} alt={movie.title} onError={handleError} />
             <div className="movie-card-info">
                 <h3 className="movie-card-title">{movie.title}</h3>
                 <p className="movie-card-genre">{movie.genre}</p>
-                <p className="movie-card-rating">Rating: {movie.rating}</p>
+                <p className={`movie-card-rating ${getRating(movie.rating)}`}>
+                    {movie.rating}</p>
             </div>
         </div>
     );
--- src/components/MovieGrid.js
+++ src/components/MovieGrid.js
@@ -1,31 +1,43 @@
 import React, { useState, useEffect } from "react";
-import '../styles.css';
+import "../styles.css";
 import MovieCard from "./MovieCard";
 
 export default function MovieGrid() {
-    const [movies, setMovies] = useState([]);
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleSearchChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const filteredMovies = movies.filter((movie) =>
+    movie.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
 
 
-    useEffect(() => {
+  useEffect(() => {
+    fetch("movies.json")
+      .then((response) => response.json())
+      .then((data) => {
+        setMovies(data);
+      });
+  }, []);
 
-        fetch("movies.json")
-            .then((response) => response.json())
-            .then((data) => {
-                setMovies(data);
-            })
-
-
-
-    }, []);
-
-    return (
-
-        <div className='movies-grid'>
-            {movies.map((movie) => (
-                <MovieCard movie={movie} key={movie.id} ></MovieCard> 
-
-
-            ))}</div>)
-};
-
+  return (
+    <div>
+      <input
+        type="text"
+        className="search-input"
+        placeholder="Search movies..."
+        value={searchTerm}
+        onChange={handleSearchChange}
+      />
+      <div className="movies-grid">
+        {filteredMovies.map((movie) => (
+          <MovieCard movie={movie} key={movie.id}></MovieCard>
+        ))}
+      </div>
+    </div>
+  );
+}
