c3147cdd7973e9ab4a498ca895e39ae1132117c7
Fimil Faneea <fimil.faneea@gds.ey.com>
2025-05-15 14:35:37 +0530

    router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import MovieGrid from "./components/MovieGrid";
+import WatchList from "./components/WatchList";
 
-import './App.css';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import MovieGrid from './components/MovieGrid';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <div className='container'>
-
+      <div className="container">
         <Header></Header>
-        <MovieGrid></MovieGrid>
+        <Router>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/watchlist">Watch List</Link>
+              </li>
+            </ul>
+          </nav>
+          <Routes>
+            <Route path="/" element={<MovieGrid />}></Route>
+            <Route path="/watchlist" element={<WatchList />}></Route>
+          </Routes>
+        </Router>
+
         <Footer></Footer>
       </div>
-
     </div>
   );
 }
---
+++ src/components/WatchList.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "../styles.css";
+
+export default function WatchList() {
+    return (
+        <div className="title">
+           <h1>Your WatchList</h1>
+        </div>
+    );
+
+} 
