188bb7b4f0d4c4bbca36ae207c987f6261d5d1ee
Fimil Faneea <fimil.faneea@gds.ey.com>
2025-05-17 16:34:58 +0530

    Combined reducers

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
+const redux = require("redux");
+const createStore = redux.createStore;
+const combineReducers = redux.combineReducers;
+
 const BUY_CAKE = "BUY_CAKE";
+const BUY_ICECREAM = "BUY_ICECREAM";
 
 function buyCake() {
   return {
@@ -7,20 +12,57 @@ function buyCake() {
   };
 }
 
-const initialState = {
-    numOfCakes: 10,
+function buyIceCream() {
+  return {
+    type: BUY_ICECREAM,
+    info: "First redux action",
+  };
 }
 
-const cakeReducer = (state = initialState, action) => {
+const initialCakeState = {
+  numOfCakes: 10,
+};
 
-    switch (action.type) {
-        case BUY_CAKE:
-            return {
-                ...state,
-                numOfCakes: state.numOfCakes - 1,
-            }
-        default:
-            return state;
-    }   
+const initialIceCreamState = {
+  numOfIceCreams: 20,
+};
 
-}
+const cakeReducer = (state = initialCakeState, action) => {
+  switch (action.type) {
+    case BUY_CAKE:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes - 1,
+      };
+    default:
+      return state;
+  }
+};
+
+const iceCreamReducer = (state = initialIceCreamState, action) => {
+  switch (action.type) {
+    case "BUY_ICECREAM":
+      return {
+        ...state,
+        numOfIceCreams: state.numOfIceCreams - 1,
+      };
+    default:
+      return state;
+  }
+};
+
+const rootReducer = combineReducers({
+  cake: cakeReducer,
+  iceCream: iceCreamReducer,
+});
+const store = createStore(rootReducer);
+console.log("Initial state", store.getState());
+const unsubscribe = store.subscribe(() =>
+  console.log("Updated state", store.getState())
+);
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+store.dispatch(buyIceCream());
+store.dispatch(buyIceCream());
+unsubscribe();
