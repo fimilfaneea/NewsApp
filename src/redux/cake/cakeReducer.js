f37ec8c0a88f41841aa1b65cb4942d08cadf4141
Fimil Faneea <fimil.faneea@gds.ey.com>
2025-05-18 07:57:49 +0530

    React_File_Structure

---
+++ src/redux/cake/cakeReducer.js
@@ -0,0 +1,17 @@
+import { BUY_CAKE } from "./cakeType";
+const inititialState = {
+  numOfCakes: 10,
+};
+
+const cakeReducer = (state = inititialState, action) => {
+  switch (action.type) {
+    case BUY_CAKE:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes - 1,
+      };
+    default:
+      return state;
+  }
+};
+ export default cakeReducer;
